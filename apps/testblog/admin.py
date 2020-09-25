7e42ddda6383becf0eea0ac00d4f858c138d51a4
kanwulf01 <radamanthyzdark@hotmail.com>
2020-09-24 21:34:08 -0500

    import_export works

--- apps/testblog/admin.py
+++ apps/testblog/admin.py
@@ -1,15 +1,32 @@
 from django.contrib import admin
 from .models import *
+from import_export import resources
+from import_export.admin import ImportExportModelAdmin
 # Register your models here.
 
-class CategoriaAdmin(admin.ModelAdmin):
+class CategoriaResource(resources.ModelResource):
+
+    class Meta:
+        model = Categoria
+
+
+
+class AuthorResource(resources.ModelResource):
+
+    class Meta:
+        model = Autor
+
+
+class CategoriaAdmin(ImportExportModelAdmin,admin.ModelAdmin):
 
     search_fields = ['nombre']
     list_display = ('nombre','estado','fecha_creacion')
+    resource_class = CategoriaResource
 
-class AuthorAdmin(admin.ModelAdmin):
+class AuthorAdmin(ImportExportModelAdmin ,admin.ModelAdmin):
     search_fields = ['nombres']
     list_display = ('nombres','apellidos','facebook','twitter','instagram', 'web','correo','estado','fecha_creacion',)
+    resource_class = AuthorResource
     
 
 #register recive 2 parametros nombre del modelo y una clase que herede de categoriaadmin para visualizar instancias del modelo
--- core/settings.py
+++ core/settings.py
@@ -38,6 +38,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'apps.testblog',
+    'import_export',
 ]
 
 MIDDLEWARE = [
