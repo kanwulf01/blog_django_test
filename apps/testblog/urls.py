143ca6201c9c6e4e02094283fa6c68af10036cd1
kanwulf01 <radamanthyzdark@hotmail.com>
2020-09-25 14:33:12 -0500

    templates statics

--- apps/testblog/urls.py
+++ apps/testblog/urls.py
@@ -1,6 +1,12 @@
 from django.urls import path
-from .views import home
+from .views import home, general, tecnologia, programacion, videojuegos, tutoriales
 
 urlpatterns = [
     path('', home, name="index"),
+    path('general/', general, name='general'),
+    path('tecnologia/', tecnologia, name='tecnologia'),
+    path('programacion/', programacion, name='programacion'),
+    path('videojuegos/', videojuegos, name='videojuegos'),
+    path('tutoriales/', tutoriales, name='tutoriales'),
+
 ]
--- apps/testblog/views.py
+++ apps/testblog/views.py
@@ -4,3 +4,21 @@ from django.shortcuts import render
 
 def home(request):
     return render(request, 'index.html')
+
+def general(request):
+    return render(request, 'general.html')
+
+def programacion(request):
+    return render(request, 'programacion.html')
+
+def tutoriales(request):
+    return render(request, 'tutoriales.html')
+
+def tecnologia(request):
+    return render(request, 'tecnologia.html')
+
+def videojuegos(request):
+    return render(request, 'videojuegos.html')
+
+
+
